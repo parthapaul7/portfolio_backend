dbea6a5b31e6a19f4510a68516455aa48597255f
Partha Paul <pparthapaul11@gmail.com>
2022-08-14 00:25:19 +0530

    sttic files

--- portfolio_backend/urls.py
+++ portfolio_backend/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('portfolio/', include('cms_portfolio.urls')),
 ]
-urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += staticfiles_urlpatterns()
